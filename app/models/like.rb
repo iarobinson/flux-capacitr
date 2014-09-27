9672644bcd88a3d4b51b5cdb7c89fffee0c4bf08
Ryan Glassett <rglassett@gmail.com>
2014-09-26 17:09:01 -0700

    Validated that user cannot like own posts

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,14 @@
 class Like < ActiveRecord::Base
-  validates :user_id, :post_id, presence: true
-  validates :user_id, uniqueness: { scope: :post_id }
+  validates :user, :post, presence: true
+  validates :user, uniqueness: { scope: :post_id }
+  validate :user_cannot_like_own_post
   
   belongs_to :user
   belongs_to :post
+  
+  def user_cannot_like_own_post
+    if self.user == self.post.author
+      errors.add("You cannot like your own post")
+    end
+  end
 end
