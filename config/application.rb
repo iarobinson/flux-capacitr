3d3b05a59c5d6644be3032317ba373b05b785e40
Ryan Glassett <rglassett@gmail.com>
2014-10-01 21:35:18 -0700

    Attempted heroku bugfix

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Allonsy
   class Application < Rails::Application
     config.filepicker_rails.api_key = "A6ihAfREPRVyLX2xLPh9rz"
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
