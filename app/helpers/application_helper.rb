5e94d24affab1212a67b72e5a6eab82a970a38ff
Ryan Glassett <rglassett@gmail.com>
2014-10-02 17:12:21 -0700

    Added random backgrounds

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,18 @@ module ApplicationHelper
     HTML
     html.html_safe
   end
+  
+  def random_bg
+    images = [
+      "/images/cover.jpg",
+      "/images/cover2.jpg",
+      "/images/cover3.jpg",
+      "/images/cover4.jpg",
+      "/images/cover5.jpg",
+      "/images/cover6.jpg",
+      "/images/cover7.jpg",
+      "/images/cover8.jpg"
+    ]
+    images.sample
+  end
 end
