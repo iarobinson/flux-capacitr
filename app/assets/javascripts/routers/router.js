c311b6844cceb8345c1913621ce2e58b358c9dc8
Ryan Glassett <rglassett@gmail.com>
2015-03-09 09:36:31 -0700

    Remove extraneous Backbone routers

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -1,14 +1,22 @@
-Allonsy.BlogRouter = Backbone.Router.extend(
+Allonsy.Router = Backbone.Router.extend(
   _.extend({}, Allonsy.Mixins.SwappingRouter, {
     routes: {
+      '': 'feedShow',
       'blogs/:id': 'blogShow',
-      'posts/:id': 'postShow'
+      'posts/:id': 'postShow',
+      'search/:searchString': 'searchShow'
     },
 
     blogShow: function (id) {
       var blog = Allonsy.Collections.blogs.getOrFetch(id);
       blog.posts().fetch();
       var view = new Allonsy.Views.BlogShow({ model: blog });
+      this._swapView(view);
+    },
+
+    feedShow: function () {
+      var feed = new Allonsy.Models.Feed();
+      var view = new Allonsy.Views.FeedShow({ model: feed });
       this._swapView(view);
     },
 
@@ -26,6 +34,12 @@ Allonsy.BlogRouter = Backbone.Router.extend(
           router._swapView(view);
         }
       });
+    },
+
+    searchShow: function (searchString) {
+      var search = new Allonsy.Models.Search({ searchString: searchString });
+      var view = new Allonsy.Views.SearchShow({ model: search });
+      this._swapView(view);
     }
   })
 );
