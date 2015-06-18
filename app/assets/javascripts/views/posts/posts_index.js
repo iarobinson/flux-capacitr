54f3d36b1985c8b4b4b6418e9fa25a24b6b5626b
Ryan Glassett <rglassett@gmail.com>
2015-06-18 01:28:38 -0700

    Add PostsIndex view

---
+++ app/assets/javascripts/views/posts/posts_index.js
@@ -0,0 +1,33 @@
+Allonsy.Views.PostsIndex = Backbone.CompositeView.extend(
+  _.extend({}, Allonsy.Mixins.PaginatedView, {
+    template: JST['posts/index'],
+
+    initialize: function () {
+      this.listenTo(this.collection, "add", this.addPost);
+      this.listenTo(this.collection, "remove", this.removePost);
+      this.collection.each(this.addPost.bind(this));
+
+      setInterval(this.nextResults.bind(this), 1000);
+    },
+
+    addPost: function (post) {
+      var subview = new Allonsy.Views.PostsIndexItem({ model: post });
+      this.addSubview(".posts", subview);
+    },
+
+    nextResults: function () {
+      this.nextPage(this.collection);
+    },
+
+    removePost: function (post) {
+      this.removeModelSubview(".posts", post);
+    },
+
+    render: function () {
+      var renderedContent = this.template();
+      this.$el.html(renderedContent);
+      this.attachSubviews();
+      return this;
+    }
+  })
+);
